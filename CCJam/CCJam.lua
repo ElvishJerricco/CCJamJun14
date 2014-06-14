6ffbaa6eeed52682f56cd90799b906cc0d7d26f2
ElvishJerricco <elvishjerricco@gmail.com>
2014-06-13 22:54:27 -0400

    Comments and assert that Modules subclass Module

--- CCJam/CCJam.lua
+++ CCJam/CCJam.lua
@@ -1,5 +1,11 @@
+-- Import classes
+local Monitor = require("Monitor.lua")
+local Module = require("Module.lua")
+
+-- Root directory
 local root = fs.getDir(shell.getRunningProgram())
 
+-- Utility; The way fs.combine SHOULD behave
 local function combine(str1, str2, ...)
 	if not str2 then
 		return str1
@@ -7,6 +13,7 @@ local function combine(str1, str2, ...)
 	return fs.combine(str1, combine(str2, ...))
 end
 
+-- Utility; Get the config named: name
 local function getConfig(name)
 	local file = assert(
 		fs.open(
@@ -22,11 +29,11 @@ local function getConfig(name)
 	return textutils.unserialize(file.readAll())
 end
 
+-- Load configs
 local monitorConfig = getConfig("monitors.cfg")
 local serverConfig = getConfig("server.cfg")
 
-
-local Monitor = require("Monitor.lua")
+-- Load all the monitors requested by the config
 local monitors = {}
 table.insert(monitors, ||Monitor new| initWithEventParameters:"mouse_click", 1|)
 
@@ -34,10 +41,11 @@ for k,v in pairs(monitorConfig) do
 	table.insert(monitors, ||Monitor new| initWithEventParameters:"monitor_touch", k|)
 end
 
-
+-- Load modules
 local modules = {}
 for i,v in ipairs(fs.list(combine(root, "modules"))) do
-	local ModuleClass = require(combine("modules", v))
+	local ModuleClass = require(fs.combine("modules", v))
+	assert(ModuleClass and |ModuleClass isSubclassOf:Module|, "Modules must return a subclass of Module")
 	local module = ||ModuleClass new| init|
 	table.insert(modules, module)
 end
@@ -45,3 +53,5 @@ end
 for i,v in ipairs(modules) do
 	|v loadModule|
 end
+
+-- Link modules and monitors
