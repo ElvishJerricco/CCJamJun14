2690acaf43fa121e30d30ab32c518ab9443dbffe
ElvishJerricco <elvishjerricco@gmail.com>
2014-06-15 16:57:34 -0400

    System now displays all drives

--- CCJam/modules/System.lua
+++ CCJam/modules/System.lua
@@ -5,7 +5,9 @@ local function getUsedSpace(path)
 	for i,v in ipairs(fs.list(path)) do
 		local full = fs.combine(path, v)
 		if fs.isDir(full) then
-			total = total + getUsedSpace(full)
+			if fs.getDrive(full) == fs.getDrive(path) then
+				total = total + getUsedSpace(full)
+			end
 		else
 			total = total + fs.getSize(full)
 		end
@@ -19,20 +21,37 @@ return @class:require("../Module.lua")
 	end
 
 	function (drawInWindow:win)
-		local freeSpace = fs.getFreeSpace("")
-		local usedSpace = getUsedSpace("")
-		local totalSpace = usedSpace + freeSpace
+		local drives = {{name="HDD",used=getUsedSpace(""),free=fs.getFreeSpace("")}}
+		for i,v in ipairs(fs.list("")) do
+			if fs.getDrive(v) ~= fs.getDrive("") and not fs.isReadOnly(v) then
+				table.insert(drives, {name=v:sub(1,1):upper()..v:sub(2),used=getUsedSpace(v),free=fs.getFreeSpace(v)})
+			end
+		end
+
+		local w,h = win.getSize()
+		local thin = #drives * 3 > h
+
+		local fg, bg, tc = colors.yellow, colors.gray, colors.black
+		local function toggleColors()
+			if fg == colors.yellow then
+				fg, bg, tc = colors.lightGray, colors.gray, colors.white
+			else
+				fg, bg, tc = colors.yellow, colors.gray, colors.black
+			end
+		end
 
 		win.setBackgroundColor(colors.black)
-		win.clear()
 		win.setCursorPos(1,1)
-		gu.drawBarInWindow(
-			win,
-			string.format("HDD %d / %d", usedSpace, totalSpace),
-			usedSpace / totalSpace,
-			colors.lightGray,
-			colors.gray,
-			colors.white
-		)
+		win.clear()
+		for i,v in ipairs(drives) do
+			gu.drawBarInWindow(
+				win,
+				v.name,
+				v.used / (v.used + v.free),
+				fg, bg, tc,
+				thin
+			)
+			toggleColors()
+		end
 	end
 end
